0a133dd2e1a12ed0b49306a869ff3fd2fac71b45
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-08-01 23:11:38 +0530

    C started comments

---
+++ first.c
@@ -0,0 +1,21 @@
+/*C programming is a compiled language
+  this means that all syntax has to be legal in order to create an executable file
+C is one of the oldest languages still used today
+  Inspired numerous other languages (C++, Java)
+C is statically typed (all variables are given a type at compile time)
+C is not forgiving, simple mistakes are not told to us
+C is dangerous but helps you learn the details and behind the scenes
+C is valuable still in demand foundational concepts help everyone
+C is hard highest learning curve next to assembly
+C is not too hard once you get the hang of it
+C is low level language console apps and working with memory directly
+C is a very popular language for system programmers or low level programmers
+C has all the same programming structures used in other languages
+--variables and data types
+--operators
+--control flow statements
+--arrays and strings
+--functions
+--structs
+--pointers and memory
+*/
